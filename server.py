ed817096826bb96c08172547667c589f75cf2d02
AIeatinghamster <vito.p.jokic@gmail.com>
2026-06-27 22:29:32 +0200

    Update server.py

--- server.py
+++ server.py
@@ -2,12 +2,12 @@ from flask import Flask, request
 import os
 app = Flask(__name__)
 
-lozinka = os.environ.get("LOZINKA")
+
 
 @app.route("/server_lozinka")
 def rce():
     at = int(request.args.get("lozinka"))
-
+    lozinka = os.environ.get("LOZINKA")
     if int(lozinka) == int(at):
         return "1"
     return "0"
