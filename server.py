64ede79640ee9e3b214722e86536220b0903849f
AIeatinghamster <vito.p.jokic@gmail.com>
2026-06-27 21:47:41 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,13 @@
-#tvoje raćunalo
 from flask import Flask, request
-import os
-import subprocess
+
 app = Flask(__name__)
-lozinka=123
+
+lozinka = 123
+
 @app.route("/server_lozinka")
 def rce():
     at = int(request.args.get("lozinka"))
-    print(type(at))
-    if int(lozinka)==int(at):
-        return '1'
-    else:
-        return '0'
-app.run(host="127.0.0.1", port=8080)
+
+    if lozinka == at:
+        return "1"
+    return "0"
